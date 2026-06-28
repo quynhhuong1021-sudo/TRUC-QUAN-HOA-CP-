596c3cfc7704f12ecb9ac1086951186fe35fd4a8
quynhhuong1021-sudo <quynhhuong1021@gmail.com>
2026-06-28 16:10:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ st.image("logo.jpg")
 # TIÊU ĐỀ
 # =============================
 st.title("📈 TRỰC QUAN HÓA GIÁ CỔ PHIẾU VÀ KIỂM ĐỊNH MANN-KENDALL")
-st.subheader("Nguyễn Hoàng Quỳnh Hương_đề tài 3")
+st.subheader("TS. VŨ ĐỨC BÌNH")
 
 st.markdown("---")
 
@@ -136,8 +136,8 @@ if run:
     ax[1].set_title("Log Return")
     ax[1].set_ylabel("Return")
     ax[1].set_xlabel("Date")
-  ax[1].legend()
-    ax[1].grid(True)
+    ax[1].legend()
+ax[1].grid(True)
 
     plt.tight_layout()
 
